6d848b78cb0526e9544aedc188aa6e395f7d07ce
MaxBasharin <112333199+MaxBasharin@users.noreply.github.com>
2022-12-17 23:46:59 +0300

    header, footer, top-slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
-import * as showMessageWB from './modules/functions.js'
-showMessageWB.showMessage()
+
+$(function () {
+    $('.header__btn').on('click', function (){
+        $('.rightside-menu').removeClass('rightside-menu--close')
+    })
+    $('.rightside-menu__close').on('click', function (){
+        $('.rightside-menu').addClass('rightside-menu--close')
+    })
+})
+
+$('.top__slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
