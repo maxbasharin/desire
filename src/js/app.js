c8208ce97ce970a6faa92fbd44df933413bfcb70
MaxBasharin <112333199+MaxBasharin@users.noreply.github.com>
2023-02-04 15:08:26 +0300

    all

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,28 @@
 
 $(function () {
-    $('.header__btn').on('click', function (){
+    $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close')
     })
-    $('.rightside-menu__close').on('click', function (){
+    $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
     })
+
+
+    $('.top__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
 })
 
-$('.top__slider').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: true,
-  fade: true,
-  autoplay: true,
-  autoplaySpeed: 2000
+var mixer = mixitup('.gallery__inner', {
+    load: {
+        filter: '.bedroom'
+    }
 });
